1e4b5e20aa4b9f14428ffa0aa81f8480934eb66f
kretsulaksusha <kretsula.pn@ucu.edu.ua>
2023-10-12 17:31:15 +0300

    Constructor modified
    In FlowerPack.java assigning a copy value to the parameter flower.

--- src/main/java/flower/FlowerPack.java
+++ src/main/java/flower/FlowerPack.java
@@ -15,7 +15,7 @@ public class FlowerPack {
     }
 
     public FlowerPack(Flower flower, int quantity) {
-        this.flower = flower;
+        this.flower = new Flower(flower);
         this.quantity = quantity;
     }
 
