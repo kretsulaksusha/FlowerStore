77b814e444dc43d5419e86b22421ca241b8aa6fd
kretsulaksusha <kretsula.pn@ucu.edu.ua>
2023-10-12 14:01:32 +0300

    doc added

--- src/test/java/flower/store/FlowerBucketTest.java
+++ src/test/java/flower/store/FlowerBucketTest.java
@@ -1,3 +1,6 @@
+/**
+ * Flower package for creating flowers.
+ */
 package flower.store;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -12,17 +15,26 @@ import org.junit.jupiter.api.Assertions;
 import java.util.Random;
 
 public class FlowerBucketTest {
+    // creating renerator
     private static final Random RANDOM_GENERATOR = new Random();
+    // setting max quantity
     private static final int MAX_QUANTITY = 1000;
+    // setting max price
     private static final int MAX_PRICE = 100;
 
     private FlowerBucket flowerBucket;
 
+    /**
+     * Initializing FlowerBucket
+     */
     @BeforeEach
     public void init() {
         flowerBucket = new FlowerBucket();
     }
 
+    /**
+     * Testing getPrice method in FlowerBucket
+     */
     @Test
     public void testPrice() {
         int price = RANDOM_GENERATOR.nextInt(MAX_PRICE);
